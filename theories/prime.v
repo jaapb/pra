fd0a6b3b91d6ff2664ec82da58d42b7dea310fe3
Jaap Boender <jaapb@kerguelen.org>
2015-06-25 16:11:44 +0100

    Updated prime to properly compare.

--- theories/prime.v
+++ theories/prime.v
@@ -24,7 +24,7 @@ Qed.
 Theorem prime_test4: prime 101.
 Proof.
   unfold prime. unfold divides. Time PRA.
-Qed. *)
+Qed.
 
 
 (* Primality of integers with ZArith *)
@@ -35,12 +35,12 @@ Definition zprime (n : Z) : Prop :=
   (n > 1)%Z /\ (forall d : Z,
   (d > 0)%Z /\ (d <= n)%Z -> zdivides d n -> (d = 1%Z \/ d = n)).
 
-(* Theorem zprime_test: zprime 17.
+Theorem zprime_test: zprime 61.
 Proof.
-  unfold zprime. unfold zdivides. PRA.
-Qed. *)
+  unfold zprime. unfold zdivides. Time PRA.
+Qed.
 
-(* Theorem zprime_test2: ~(zprime 18).
+Theorem zprime_test2: ~(zprime 60).
 Proof.
-  unfold zprime. unfold zdivides. PRA.
-Qed. *)
+  unfold zprime. unfold zdivides. Time PRA.
+Qed.
